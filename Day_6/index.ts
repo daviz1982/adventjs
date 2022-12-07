b3f05325c48170623f024f2168892f25f01a3ee4
daviz1982 <daviz1982@gmail.com>
2022-12-07 17:59:02 +0100

    Update index.ts

--- Day_6/index.ts
+++ Day_6/index.ts
@@ -1,12 +1,3 @@
-function getMaxGifts(giftsCities:number[], maxGifts:number, maxCities: number) {
-  giftsCities.sort((a:number,b:number):number=>a-b)
-  let usedCities = 0
-  let leftGifts = maxGifts;
-  let sum = 0
-  giftsCities.forEach((city: number) => {
-    if (city > leftGifts) return
-    leftGifts -= city
-    sum += city
-  }) 
-  return sum
+function createCube(size) {
+  return ''
 }
