9f812fea870196133a135e5734d5aea9819bfebf
daviz1982 <daviz1982@gmail.com>
2022-12-07 17:59:32 +0100

    Create index.ts

---
+++ Day_5/index.ts
@@ -0,0 +1,12 @@
+function getMaxGifts(giftsCities:number[], maxGifts:number, maxCities: number) {
+  giftsCities.sort((a:number,b:number):number=>a-b)
+  let usedCities = 0
+  let leftGifts = maxGifts;
+  let sum = 0
+  giftsCities.forEach((city: number) => {
+    if (city > leftGifts) return
+    leftGifts -= city
+    sum += city
+  }) 
+  return sum
+}
